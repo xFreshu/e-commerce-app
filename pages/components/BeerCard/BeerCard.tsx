1f3d7ecd12f1caa2ac642569cb7f6bfef8e7114b
DESKTOP-EJMKT25\Lukasz <lprzybyszcontact@gmail.com>
2022-03-07 17:40:31 +0100

    Fetch data as getInitialProps, and map array in app

---
+++ pages/components/BeerCard/BeerCard.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+const BeerCard: FunctionComponent = () => {
+    return (
+        <div>
+            <span>123</span>
+        </div>
+    );
+};
+
+export default BeerCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,38 @@
 import type { NextPage } from 'next'
+import styled from 'styled-components'
 
-const Home: NextPage = () => {
-  return <div>Test</div>
+const Card = styled.li`
+  background-color: red;
+  width: 200px;
+  height: 200px;
+  margin: 40px;
+`
+
+interface BeerProps {
+  id: number
+  name: string
+  description: string
+}
+
+const Home = ({ beers }: { beers: BeerProps[] }) => {
+  console.log(beers)
+  return (
+    <div>
+      <ul>
+        {beers.map(({ id, name, description }) => (
+          <Card key={id}>
+            {id} {name} {description}
+          </Card>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+Home.getInitialProps = async () => {
+  const res = await fetch('https://api.punkapi.com/v2/beers')
+  const beers = await res.json()
+  return { beers }
 }
 
 export default Home
