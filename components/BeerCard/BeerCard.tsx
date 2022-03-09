1fb56ee1107d86ba4918f1e7d5d97312b463011d
DESKTOP-EJMKT25\Lukasz <lprzybyszcontact@gmail.com>
2022-03-09 19:35:41 +0100

    fix type x2

--- components/BeerCard/BeerCard.tsx
+++ components/BeerCard/BeerCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Card } from './BeerCard.styles'
 //change type
-const BeerCard = ({ id, name, image_url, description }: never) => {
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const BeerCard = ({ id, name, image_url, description }) => {
   return (
     <Card key={id}>
       <span>{id}</span>
